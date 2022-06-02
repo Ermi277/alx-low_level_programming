2d5ecb340593f790a567f952f6b55166534d7d65
Ermi277 <mr.ermiyas@gmail.com>
2022-06-02 14:20:44 -0700

    task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - printf
+ *
+ * Resource: with proper grammar, but the outcome is a piece of art,
+ **/
+int main(void)
+{
+	printf("%s\n","with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}	
