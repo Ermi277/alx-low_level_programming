a522cb20b4d02ffb94b77e399ff00e37cfdbe9bf
Ermi277 <mr.ermiyas@gmail.com>
2022-06-02 15:02:12 -0700

    task6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Exit Point
+ **/
+
+int main(void)
+{
+	printf("Size of a char:\n", sizeof(charType));
+	printf("Size of an int:\n", sizeof(intType));
+	printf("Size of a long int:\n", sizeof(longintType));
+	printf("Size of a float:\n", sizeof(floatType));
+	return (0);
+}	
