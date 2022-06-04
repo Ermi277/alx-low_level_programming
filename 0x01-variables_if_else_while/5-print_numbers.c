f5aa1f77ec6aaf5907a7188b597d8a8130cbc012
Ermi277 <mr.ermiyas@gmail.com>
2022-06-04 02:42:43 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print 0-9
+ *
+ *
+ * Return: if 0 great
+ **/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
