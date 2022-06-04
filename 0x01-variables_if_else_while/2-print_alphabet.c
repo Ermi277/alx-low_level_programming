77dcef9f4a51c92f33c00e1a87465ff8b02d557b
Ermi277 <mr.ermiyas@gmail.com>
2022-06-04 02:01:21 -0700

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main- print the english alphabet
+ *
+ *
+ * Return: if 0 great
+ **/
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
