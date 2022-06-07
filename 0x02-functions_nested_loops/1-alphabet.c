e066a8fe3852ccce274d234577328a37ab888902
Ermi277 <mr.ermiyas@gmail.com>
2022-06-07 14:02:41 -0700

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -10,5 +11,17 @@
 int main(void)
 {
 	print_alphabet();
+	return (0);
+}
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
 	return (0);
 }
