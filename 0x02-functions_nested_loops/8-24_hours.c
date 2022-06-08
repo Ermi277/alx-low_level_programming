3d1447292b7b32412130e1b66718baa0eeefb1ef
Ermi277 <mr.ermiyas@gmail.com>
2022-06-08 14:08:08 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,19 +9,19 @@
 
 void jack_bauer(void)
 {
-	for (char i = 0; i < 3; i++)
+	for (int i = 0; i < 3; i++)
 	{
-		for (char j = 0; j < 4; j++)
+		for (int j = 0; j < 4; j++)
 		{
-			for (char k = 0; k < 6; k++)
+			for (int k = 0; k < 6; k++)
 			{
-				for (char l = 0; l < 10; l++)
+				for (int l = 0; l < 10; l++)
 				{
-					_putchar(i);
-					_putchar(j);
+					_putchar('0' + i);
+					_putchar('0' + j);
 					_putchar(':');
-					_putchar(k);
-					_putchar(l);
+					_putchar('0' + k);
+					_putchar('0' + l);
 					_putchar('\n');
 				}
 			}
