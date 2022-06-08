4ee28b16710cb0107ca5b33cd806e51f5afddebb
Ermi277 <mr.ermiyas@gmail.com>
2022-06-08 05:20:31 -0700

    task4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include <ctype.h>
+#include "main.h"
+
+/** _isalpha - checks alpha and upper or lowercase
+ *
+ *
+ * @c: recive an integer
+ * Return: 0
+ **/
+
+int _isalpha(int c)
+{
+	if (isalpha(c) > 1 && islower(c) < 0 || islower(c) > 0)
+		return (1);
+	else
+		return (0);
+}
