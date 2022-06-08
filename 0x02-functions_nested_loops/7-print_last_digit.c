5728fe4ee48daff203c03051ac8aaab6386652b6
Ermi277 <mr.ermiyas@gmail.com>
2022-06-08 13:43:44 -0700

    task7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,15 +3,17 @@
  * print_last_digit - print the last digit of a number
  *
  *
- *@x: recive the number
+ * @c: recive the number
  * Return: last digit
  **/
 
 int print_last_digit(int c)
 {
 	char r;
+	int x;
 
-	r = '0' + c % 10;
+	x = c % 10;
+	r = '0' + x;
 	_putchar(r);
-	return (c % 10);
-}	
+	return (x);
+}
