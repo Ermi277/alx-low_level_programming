7c229a3960b06a99f59035efa3090d21e8caa010
Ermi277 <mr.ermiyas@gmail.com>
2022-06-09 23:33:02 -0700

    task4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print most numbers
+ *
+ * Return: no return
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	while (i < 10)
+	{
+		if (i != 2 && i !=4)
+			_putchar('0' + i);
+		i++;
+	}
+}
