7be3981043d012f25f6b510a243f2a228737aaa1
Ermi277 <mr.ermiyas@gmail.com>
2022-06-10 13:23:56 -0700

    task5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -16,10 +16,15 @@ void more_numbers(void)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			_putchar('0' + j);
+			if (j > 9)
+			{
+				_putchar('0' + (j / 10));
+				_putchar('0' + (j % 10));
+			}
+			else
+				_putchar('0' + j);
 		}
 		_putchar('\n');
 		i++;
 	}
-	_putchar('\n');
 }
