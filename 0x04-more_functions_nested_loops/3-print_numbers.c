1d9afa2b08307a15aa2d8a227cb1cfcd5d22d67b
Ermi277 <mr.ermiyas@gmail.com>
2022-06-09 23:18:57 -0700

    task3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers
+ * 
+ * Return: 0
+ */
+
+void print_numbers()
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+}
