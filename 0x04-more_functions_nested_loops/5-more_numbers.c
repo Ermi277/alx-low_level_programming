786286da8446821e53e6dfa5868a8cba0f949a75
Ermi277 <mr.ermiyas@gmail.com>
2022-06-10 01:08:26 -0700

    task5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 10 times
+ *
+ * Return: no return
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < 1)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			_putchar('0' + j)
+		}
+		_putchar('\n');
+		i++;
+	}
+}
