6fa790964ba1a03a590641ef7e0c1905e9068b9e
Ermi277 <mr.ermiyas@gmail.com>
2022-06-10 06:03:43 -0700

    task7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "main.h"
 
 /**
@@ -13,7 +14,7 @@ void print_diagonal(int n)
 
 	for (i = 0; i < abs(n); i++)
 	{
-		for (j = 0; j < i;j++)
+		for (j = 0; j < i; j++)
 		{
 			_putchar(' ');
 		}
