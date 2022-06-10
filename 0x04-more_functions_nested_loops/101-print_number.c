19556c7798fdd55ff003a6aa080a9e45cddd9f39
Ermi277 <mr.ermiyas@gmail.com>
2022-06-10 15:23:11 -0700

    task12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,16 +10,16 @@ void print_number(int n)
 {
 	if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		n = -n;
 	}	
 	if (n == 0)
 	{
-		putchar('0');
+		_putchar('0');
 	}
 	if (n / 10)
 	{
 		print_number(n / 10);
 	}
-	putchar('0' + n % 10);
+	_putchar('0' + n % 10);
 }
