1a291b7a0a17d892de08ebfd14a5d489bee17970
Ermi277 <mr.ermiyas@gmail.com>
2022-06-13 16:05:37 -0700

    task8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_array - print array with comma
+ * @a: pointer
+ * @n: aray size
+ * Return: no return
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		printf("%d, ", a[i]);
+	}
+	_putchar('\n');
+}
