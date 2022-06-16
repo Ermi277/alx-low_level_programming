9f83f03e68b371841aa464288f1207a64bf4fc0d
Ermi277 <mr.ermiyas@gmail.com>
2022-06-16 03:51:14 -0700

    task1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strncat - append the source string to destination but specified by user
+ * @dest: destination string
+ * @src: source string
+ * @n: array
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(dest, src, n);
+}
