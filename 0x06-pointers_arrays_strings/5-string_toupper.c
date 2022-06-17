e63ed5ec210eec463d743e1f7a8e67b4f7703187
Ermi277 <mr.ermiyas@gmail.com>
2022-06-17 04:29:39 -0700

    task5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,7 +2,21 @@
 
 /**
  * string_toupper - to upper
- *
+ * @s: ponter character
  * Return: 0;
  */
+char *string_toupper(char *s)
+{
+	int ln = strlens(s);
+	int i = 0;
 
+	while (i < ln)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+	i++;
+	}
+	return (s);
+}
