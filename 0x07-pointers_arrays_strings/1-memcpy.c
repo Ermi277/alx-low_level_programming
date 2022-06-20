26657e7b1ea5cecdc39facce2365b641146cd63a
Ermi277 <mr.ermiyas@gmail.com>
2022-06-20 11:40:06 -0700

    task1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * _memcpy - copy 
+ * _memcpy - copy
  * @dest: destination adress
  * @src: source adress
  * Return: dest
- */ 
+ */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	int i = 0;
