78b892ca2377d88ef8d702912deb7482d254c577
Ermi277 <mr.ermiyas@gmail.com>
2022-06-22 12:56:50 +0000

    task0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - recursion first
+ * @s: string pointer
+ * Return: pointer
+ */
+
+void _puts_recursion(char *s)
+{
+	while (*s)
+	{
+	_putchar(*s);
+	s++;
+	}
+	return (0);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,3 @@
+char _putchar(int c);
+void _puts_recursion(char *s);
+
