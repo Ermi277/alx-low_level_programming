471b796b9d57a7c137947ca645581395bf083685
Ermi277 <mr.ermiyas@gmail.com>
2022-06-22 20:54:20 +0000

    tak3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * factorial - calculate factorial
+ * @n: number
+ * Return: integer
+ */
+
+int factorial(int n)
+{
+	if (n >= 0 && n <= 1)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	else
+		return (n * factorial(n - 1));
+}
