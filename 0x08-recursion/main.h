1052ba08a98a094785c688ceb5c993ed487a549f
Ermi277 <mr.ermiyas@gmail.com>
2022-06-23 17:55:17 +0000

    task6

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,7 @@
+i = 2; 
 char _putchar(int c);
 void _puts_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 void _print_rev_recursion(char *s);
+int is_prime_number(int n);
