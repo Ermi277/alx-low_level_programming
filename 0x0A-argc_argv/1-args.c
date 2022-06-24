60101a8b7f460bfb432a37e845af50e1bb16765a
Ermi277 <mr.ermiyas@gmail.com>
2022-06-24 19:55:23 +0000

    task1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print number of argument
+ * @argc: number of arg
+ * @argv: argument
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
