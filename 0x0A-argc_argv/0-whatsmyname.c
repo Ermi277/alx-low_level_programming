d84df127c8a7c4ed254355f48004236108445746
Ermi277 <mr.ermiyas@gmail.com>
2022-06-24 19:33:39 +0000

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print name
+ * @argc: number of argument
+ * @argv: argument command
+ * Return: 0
+ */
+
+int main(int argc, char argv[])
+{
+	printf("%s\n", argv);
+	return 0;
+}
