22c617bdd1f1e58b2a7935a86e7b99c56396be99
Ermi277 <mr.ermiyas@gmail.com>
2022-06-28 21:23:29 +0000

    task1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strdup - allocated string in memory
+ * @str: the string to be allocated
+ * Return: Null or pointer
+ */
+
+char *_strdup(char *str)
+{
+	char *ct = NULL;
+	unsigned int i;
+
+	if (str == NULL)
+		return (NULL);
+	ct = malloc(sizeof(char));
+	if (ct != 0)
+	{
+		for (i = 0; ct[i] != '\0'; i++)
+		{
+			ct[i] = ct[i];
+		}
+	}
+	else
+	{
+		return (NULL);
+	}
+	ct[i] = '\0';
+	return (ct);
+}
