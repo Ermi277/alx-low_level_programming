7eaecfadf52c98bf7ed5f2ff6feb27c1464d097b
Ermi277 <mr.ermiyas@gmail.com>
2022-06-28 21:58:25 +0000

    task1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,15 +10,16 @@ char *_strdup(char *str)
 {
 	char *ct = NULL;
 	unsigned int i;
-	int j;
+	int j = 0;
 
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	for (j = 0; str[j] !='\0'; j++)
+	while (str[j] != '\0')
 	{
-	}	
+		j++;
+	}
 	ct = malloc(sizeof(char) * j + 1);
 	if (ct != 0)
 	{
