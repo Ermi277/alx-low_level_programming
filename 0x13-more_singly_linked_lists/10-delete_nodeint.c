454b7744209ab336f3ff240281ae0f35f15e5651
Ermi277 <mr.ermiyas@gmail.com>
2022-07-19 19:06:31 +0000

    task10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -26,6 +26,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		if (del->next == NULL)
 			return (-1);
 		del = del->next;
+		i++;
 	}
 	tmp = del->next;
 	del->next = tmp->next;
