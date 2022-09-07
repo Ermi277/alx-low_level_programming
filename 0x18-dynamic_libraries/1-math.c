00e5204c6253c4c2dfce307e1a1f8ba9afb59eae
Ermi277 <mr.ermiyas@gmail.com>
2022-09-07 08:29:46 +0000

    bugfix

---
+++ 0x18-dynamic_libraries/1-math.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+int add(int a, int b)
+{
+	int res;
+
+	res = a + b;
+	return (res);
+}
+
+int sub(int a, int b)
+{
+	int res;
+
+	res = a - b;
+	return (res);
+}
+
+int div(int a, int b)
+{
+	int res;
+
+	res = a / b;
+	return (res);
+}
+
+int mod(int a, int b)
+{
+	int res;
+
+	res = a % b;
+	return (res);
+}
